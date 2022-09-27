97d7a71fa80fd42fba6260e1d115b67ef945654e
TYLPHE <tylphet@gmail.com>
2022-09-27 16:40:03 -0600

    initialization and trying to figure out user authentication

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# replicate
+# Replicate
 Replicate a website as a final JavaScript project
+
+## Links
+- [Assignment Description](https://www.theodinproject.com/lessons/node-path-javascript-javascript-final-project)
+
+## Summary
+
+## Challenges
+
+## Notes
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXf68hVh9ZdgM0Z9aLHoQGHaZNT3snmFk",
+  authDomain: "replicate-2458b.firebaseapp.com",
+  projectId: "replicate-2458b",
+  storageBucket: "replicate-2458b.appspot.com",
+  messagingSenderId: "617280841257",
+  appId: "1:617280841257:web:e8fc9f93d1e188ff821e38"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
